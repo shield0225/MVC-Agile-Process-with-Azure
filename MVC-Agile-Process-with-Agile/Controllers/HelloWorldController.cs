dad75558c9fa2da07e0d7346d4a3ac8561298247
shield0225 <shieldsalcedo@yahoo.com>
2024-03-17 12:40:43 -0400

    Add project files.

---
+++ MVC-Agile-Process-with-Agile/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    //public string Index()
+    //{
+    //    return "This is my default action...";
+    //}
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    // /HelloWorld/Welcome?name=Rick&numtimes=4
+    // Requires using System.Text.Encodings.Web;
+    //public string Welcome(string name, int numTimes = 1)
+    public string Welcome(string name, int ID = 1)
+    {
+        //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+}
---
+++ MVC-Agile-Process-with-Agile/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC-Agile-Process-with-Azure.Models.Movie> Movie { get; set; }
+    }
+}
